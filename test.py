902b325342a556ba18e4ccc58382ba912a2326cb
Helene Spring <helene.spring@outlook.com>
2022-01-31 13:27:31 +0100

    Implement methods for calculating area and circumference of a circle

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-print('hello world!')
-print('blabla')
-print('another thing')
+import numpy as np
+
+def surface_circle(radius):
+    """Compute the area of a circle given its radius."""
+    return np.pi*radius**2
+
+def circumference_circle(radius):
+    '''
+    Calculates the circumference of a circle for a given radius.
+    '''
+    return 2*np.pi*radius
